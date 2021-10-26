1c9bb83e230e1a6885d4d8c9b79422c027b58119
Daniel <daniel.jablonski1998@gmail.com>
2021-10-26 22:58:04 +0100

    Test for give change

--- src/main/java/org/example/Change.java
+++ src/main/java/org/example/Change.java
@@ -1,7 +1,71 @@
 package org.example;
 
 public class Change {
-    private enum coins{
-        PENNY, NICKLE, DIME, QUARTER, HALF, DOLLAR
+
+    /**
+     * Enum for coins with its value in cents
+     */
+    public enum Coins {
+        DOLLAR(100), HALF(50), QUARTER(25), DIME(10), NICKLE(5), PENNY(1);
+        private final int value_cents;
+
+        Coins(int value_cents) {
+            this.value_cents = value_cents;
+        }
+
+        public int getValue() {
+            return value_cents;
+        }
     }
+
+    public static void giveChange(int change) {
+        int amount = 0;
+        int rest = change;
+        for (Coins coin : Coins.values())
+            switch (coin) {
+                case DOLLAR:
+                    amount = rest/ coin.getValue();
+                    if (amount >= 1) {
+                        rest = rest % coin.getValue();
+                        System.out.print(amount + " dollar coin/s\n");
+                    }
+                    break;
+                case HALF:
+                    amount = rest/ coin.getValue();
+                    if (amount >= 1) {
+                        rest = rest % coin.getValue();
+                        System.out.print(amount + " half-dollar coin/s\n");
+                    }
+                    break;
+                case QUARTER:
+                    amount = rest/ coin.getValue();
+                    if (amount >= 1) {
+                        rest = rest % coin.getValue();
+                        System.out.print(amount + " quarter/s\n");
+                    }
+                    break;
+                case DIME:
+                    amount = rest/ coin.getValue();
+                    if (amount >= 1) {
+                        rest = rest % coin.getValue();
+                        System.out.print(amount + " dime/s\n");
+                    }
+                    break;
+                case NICKLE:
+                    amount = rest/ coin.getValue();
+                    if (amount >= 1) {
+                        rest = rest % coin.getValue();
+                        System.out.print(amount + " nickle/s\n");
+                    }
+                    break;
+                case PENNY:
+                    amount = rest/ coin.getValue();
+                    if (amount >= 1) {
+                        rest = rest % coin.getValue();
+                        System.out.print(amount + " penny/ies\n");
+                    }
+                    break;
+            }
+    }
+
 }
--- src/test/java/org/example/ChangeTest.java
+++ src/test/java/org/example/ChangeTest.java
@@ -1,13 +1,55 @@
 package org.example;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 
 public class ChangeTest {
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
     }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+    @Test
+    public void giveChange30cents() {
+        Change.giveChange(30);
+        assertEquals("1 quarter/s\n"+"1 nickle/s", outContent.toString().trim());
+    }
+    @Test
+    public void giveChange150cents() {
+        Change.giveChange(150);
+        assertEquals("1 dollar coin/s\n"+"1 half-dollar coin/s", outContent.toString().trim());
+    }
+    @Test
+    public void giveNoChange() {
+        Change.giveChange(0);
+        assertEquals("", outContent.toString().trim());
+    }
+    @Test
+    public void giveChange2cents() {
+        Change.giveChange(3);
+        assertEquals("3 penny/ies", outContent.toString().trim());
+    }
+    @Test
+    public void giveChange24cents() {
+        Change.giveChange(24);
+        assertEquals("2 dime/s\n4 penny/ies", outContent.toString().trim());
+    }
+
 }
